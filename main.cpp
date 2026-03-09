c194897296732d323ce9681f72dc9c79c6ceb5b9
Barsbek Azamatov <bars.moderside@gmail.com>
2026-03-08 18:00:37 -0700

    Add struct/class

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+struct Review {
+    double rating{};
+    string comment{};
+};
+
+struct Node {
+    double value{};
+    Node* next{};
+};
+
+class Movie {
+private:
+    string m_title{};
+    Node* head{};
+public:
+
+};
+
 int main() {
     return 0;
 }
